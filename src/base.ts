03a5a0ae1462dd26e1c23c9c5569e947ff4fad41
Ismail Syed <ismail.syed@shopify.com>
2017-02-25 18:50:01 -0500

    Remove merge package and use spread operator instead

--- src/base.ts
+++ src/base.ts
@@ -1,12 +1,16 @@
 // Aggregate all the tslint rules
 // See https://palantir.github.io/tslint/rules/
 
-import * as merge from 'merge';
 import {functionalityRules} from './rules/functionality';
 import {maintainabilityRules} from './rules/maintainability';
 import {styleRules} from './rules/style';
 import {typescriptRules} from './rules/typescript';
 
 module.exports = {
-  'rules': merge(functionalityRules, maintainabilityRules, styleRules, typescriptRules),
+  'rules': {
+    ...functionalityRules,
+    ...maintainabilityRules,
+    ...styleRules,
+    ...typescriptRules,
+  },
 };
