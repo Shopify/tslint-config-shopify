81f58fd15976f4dff0eeecc42cfed879a25c3969
Ismail Syed <ismail.syed@shopify.com>
2017-03-13 12:09:13 -0400

    Clean error wording and use footnote definition in test cases

--- src/customRules/importTypeOrderRule.ts
+++ src/customRules/importTypeOrderRule.ts
@@ -15,7 +15,7 @@ export class Rule extends Lint.Rules.AbstractRule {
   };
   /* tslint:enable:object-literal-sort-keys */
 
-  public static STRUCTURED_IMPORTS_ABS_FIRST_ERROR = 'Imports should be listed in the following order: module imports, absolute imports, relative imports.';
+  public static STRUCTURED_IMPORTS_ABS_FIRST_ERROR = 'Imports should be listed in the following order: module imports, absolute imports, ancestor imports, sibling imports.';
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
     const structuredImportsWalker = new StructuredImportsWalker(sourceFile, this.getOptions());
