46c850efb7e313a82521e5b9ce8fbf52b9329810
Ismail Syed <ismail.syed@shopify.com>
2017-03-13 22:12:03 -0400

    Utilize applyWithFunction() instead of applyWithWalker()

--- src/customRules/importTypeOrderRule.ts
+++ src/customRules/importTypeOrderRule.ts
@@ -1,6 +1,7 @@
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
 
+
 export class Rule extends Lint.Rules.AbstractRule {
   /* tslint:disable:object-literal-sort-keys */
   public static metadata: Lint.IRuleMetadata = {
@@ -18,8 +19,7 @@ export class Rule extends Lint.Rules.AbstractRule {
   public static IMPORT_TYPE_ORDER_ERROR = 'Imports should be listed in the following order: module imports, absolute imports, ancestor imports, sibling imports.';
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    const structuredImportsWalker = new StructuredImportsWalker(sourceFile, this.ruleName, undefined);
-    return this.applyWithWalker(structuredImportsWalker);
+    return this.applyWithFunction(sourceFile, walker);
   }
 }
 
@@ -32,28 +32,27 @@ enum ImportType {
 
 const importStuctureOrder = [ImportType.External, ImportType.Absolute, ImportType.Ancestor, ImportType.Sibling];
 
-class StructuredImportsWalker extends Lint.AbstractWalker<void>  {
-  public walk(sourceFile: ts.SourceFile) {
-    const importNodes = sourceFile.statements
-      .filter((child) => child.kind === ts.SyntaxKind.ImportDeclaration)
-      .map((child) => child as ts.ImportDeclaration);
+function walker(context: Lint.WalkContext<void>): void {
+  const { sourceFile } = context;
+  const importNodes = sourceFile.statements
+    .filter((child) => child.kind === ts.SyntaxKind.ImportDeclaration)
+    .map((child) => child as ts.ImportDeclaration);
 
-    if (importNodes.length === 0) { return; }
+  if (importNodes.length === 0) { return; }
 
-    let previousImport = getImportType(importNodes.shift().moduleSpecifier.getText());
-    while (importNodes.length) {
-      const currentImportNode = importNodes.shift().moduleSpecifier;
-      const currentImport = getImportType(currentImportNode.getText());
-      if (previousImport > currentImport) {
-        const errorStart = currentImportNode.getStart();
-        const errorWidth = currentImportNode.getEnd() - errorStart;
-        this.addFailureAt(errorStart, errorWidth, Rule.IMPORT_TYPE_ORDER_ERROR);
-      } else {
-        previousImport = currentImport;
-      }
+  let previousImport = getImportType(importNodes.shift().moduleSpecifier.getText());
+  while (importNodes.length) {
+    const currentImportNode = importNodes.shift().moduleSpecifier;
+    const currentImport = getImportType(currentImportNode.getText());
+    if (previousImport > currentImport) {
+      const errorStart = currentImportNode.getStart();
+      const errorWidth = currentImportNode.getEnd() - errorStart;
+      context.addFailureAt(errorStart, errorWidth, Rule.IMPORT_TYPE_ORDER_ERROR);
+    } else {
+      previousImport = currentImport;
     }
   }
-};
+}
 
 function getImportType(path: string): ImportType {
   if (path.substr(1, 2) === './') {
@@ -64,4 +63,4 @@ function getImportType(path: string): ImportType {
     return ImportType.Absolute;
   }
   return ImportType.External;
-}
+};
